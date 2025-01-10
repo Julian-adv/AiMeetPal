edde4bceec714dd797de393cf08926cbee8dd839
Julian <julian.adventerer.dev@gmail.com>
2025-01-10 19:55:38 +0900

    Move api fetch from App.svelte to app.py

--- backend/app.py
+++ backend/app.py
@@ -1,30 +1,26 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
+import httpx
 from pydantic import BaseModel
+import json
 import os
 import random
-import httpx
-import json
-from typing import Optional, List, Union
-
-os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # hide tensorflow warnings
-from dotenv import load_dotenv
-# from openai import OpenAI
-from typing import Optional
-import shutil
+import base64
+from io import BytesIO
 from PIL import Image
 from impact.face_detailer import FaceDetailer, tensor2pil
 from impact.subcore import UltraBBoxDetector
-import base64
-from io import BytesIO
-from ultralytics import YOLO
 import comfy.sd
-from impact.utils import pil2tensor
 from comfyui.nodes import KSampler, EmptyLatentImage, VAEDecodeTiled
+import shutil
+import uvicorn
+from ultralytics import YOLO
+from dotenv import load_dotenv
+from typing import Optional
 
-# 1girl,maid,large breasts,christian louboutin highheels, garter straps,stockings,collar,kpop idol,long legs,narrow waist,long hair,ass,cameltoe,bent over
-
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # hide tensorflow warnings
 load_dotenv()
 
 app = FastAPI()
@@ -55,7 +51,7 @@ if not os.path.exists(UPLOAD_FOLDER):
 
 
 class ChatMessage(BaseModel):
-    message: str
+    prompt: str
 
 
 class ImageGenerationRequest(BaseModel):
@@ -101,85 +97,125 @@ async def chat(message: ChatMessage):
                 "infermaticAiApiKey": "your_api_key_here"
             }
 
-    try:
-        payload = {
-            "prompt": message.message,
-            "model": settings["model"],
-            "max_new_tokens": settings["max_new_tokens"],
-            "max_tokens": settings["max_tokens"],
-            "temperature": settings["temperature"],
-            "top_p": settings["top_p"],
-            "typical_p": settings["typical_p"],
-            "typical": settings["typical"],
-            "sampler_seed": -1,
-            "min_p": 0.02,
-            "repetition_penalty": 1,
-            "frequency_penalty": 0,
-            "presence_penalty": 0,
-            "top_k": -1,
-            "skew": 0,
-            "min_tokens": 0,
-            "add_bos_token": True,
-            "smoothing_factor": 0,
-            "smoothing_curve": 1,
-            "dry_allowed_length": 2,
-            "dry_multiplier": 0.75,
-            "dry_base": 1.75,
-            "dry_sequence_breakers": '["\\n",":","\\"","*"]',
-            "dry_penalty_last_n": 0,
-            "max_tokens_second": 0,
-            "stopping_strings": [
-                "\nJulien:",
-                "\nStellar:",
-                "<|eot_id|>",
-                "<|start_header_id|>writer character: Julien<|end_header_id|>",
-                "<|start_header_id|>writer character: Jessica<|end_header_id|>"
-            ],
-            "stop": [
-                "\nJulien:",
-                "\nStellar:",
-                "<|eot_id|>",
-                "<|start_header_id|>writer character: Julien<|end_header_id|>",
-                "<|start_header_id|>writer character: Jessica<|end_header_id|>"
-            ],
-            "truncation_length": 8192,
-            "ban_eos_token": False,
-            "skip_special_tokens": True,
-            "top_a": 0,
-            "tfs": 1,
-            "mirostat_mode": 0,
-            "mirostat_tau": 5,
-            "mirostat_eta": 0.1,
-            "custom_token_bans": "",
-            "banned_strings": [],
-            "api_type": "infermaticai",
-            "api_server": "https://api.totalgpt.ai",
-            "xtc_threshold": 0.1,
-            "xtc_probability": 0.5,
-            "nsigma": 0,
-            "n": 1,
-            "ignore_eos": False,
-            "spaces_between_special_tokens": True,
-            "stream": True
-        }
-        
+    async def generate():
         async with httpx.AsyncClient() as client:
-            response = await client.post(
+
+            prompt = ('<|start_header_id|>system<|end_header_id|>\n' +
+                '\n' +
+                'Develop the plot slowly, always stay in character. Describe all actions in full, elaborate, explicit, graphic, and vivid detail. Mention all relevant sensory perceptions. Keep the story immersive and engaging.\n' +
+                '\n' +
+                '\n' +
+                '## Overall plot description:\n' +
+                '\n' +
+                'Conversation between Stellar and Julien.\n' +
+                '\n' +
+                '\n' +
+                '## Characters:\n' +
+                '\n' +
+                '### Stellar\n' +
+                '\n' +
+                '[Stellar\'s Personality= "loyal", "cheerful", "intelligent", "smart", "wise", "mischievous", "proactive", "young (20 years old)"]\n' +
+                '[Stellar\'s body= "silver-blonde hair", "long straight hair", "turquoise eyes", "long eyelashes", "white teeth", "pink plump glossy lips", "milky white skin", "porcelain-smooth skin", "large breasts (32F cup)", "firm breasts that seem to defy gravity", "long, slender legs", "slender waist (56cm, 22in)", "tight buttocks", "small feet", "long slender fingers", "tall height (175cm, 5\'9)"]\n' +
+                "[Genre: romance fantasy; Tags: adult; Scenario: Stellar is working at Julien's mansion as a head maid.]\n" +
+                '\n' +
+                '### Julien\n' +
+                '\n' +
+                'Julien is living alone in a luxury mansion.\n' +
+                '\n' +
+                'The first floor of the mansion includes the master bedroom, master bathroom, living room, kitchen, family room, and study. On the second floor, there are rooms for the maids.<|eot_id|>\n' +
+                '<|start_header_id|>user<|end_header_id|>\n' +
+                '\n' +
+                'Start the role-play between Stellar and Julien.\n' +
+                '<|eot_id|>\n' +
+                '<|start_header_id|>writer character: Stellar<|end_header_id|>\n' +
+                '\n' +
+                'Good morning. Master. Jessica, your new maid candidate, has arrived and is waiting for you. Shall I bring her here?<|eot_id|>\n' +
+                '<|start_header_id|>writer character: Julien<|end_header_id|>\n' +
+                '\n' +
+                f'{message.prompt}<|eot_id|>\n')
+
+            payload = {
+                "prompt": prompt,
+                "model": settings["model"],
+                "max_new_tokens": settings["max_new_tokens"],
+                "max_tokens": settings["max_tokens"],
+                "temperature": settings["temperature"],
+                "top_p": settings["top_p"],
+                "typical_p": settings["typical_p"],
+                "typical": settings["typical"],
+                "sampler_seed": -1,
+                "min_p": 0.02,
+                "repetition_penalty": 1,
+                "frequency_penalty": 0,
+                "presence_penalty": 0,
+                "top_k": -1,
+                "skew": 0,
+                "min_tokens": 0,
+                "add_bos_token": True,
+                "smoothing_factor": 0,
+                "smoothing_curve": 1,
+                "dry_allowed_length": 2,
+                "dry_multiplier": 0.75,
+                "dry_base": 1.75,
+                "dry_sequence_breakers": '["\\n",":","\\"","*"]',
+                "dry_penalty_last_n": 0,
+                "max_tokens_second": 0,
+                "stopping_strings": [
+                    "\nJulien:",
+                    "\nStellar:",
+                    "<|eot_id|>"
+                ],
+                "stop": [
+                    "\nJulien:",
+                    "\nStellar:",
+                    "<|eot_id|>"
+                ],
+                "truncation_length": 8192,
+                "ban_eos_token": False,
+                "skip_special_tokens": True,
+                "top_a": 0,
+                "tfs": 1,
+                "mirostat_mode": 0,
+                "mirostat_tau": 5,
+                "mirostat_eta": 0.1,
+                "custom_token_bans": "",
+                "banned_strings": [],
+                "api_type": "infermaticai",
+                "api_server": "https://api.totalgpt.ai",
+                "xtc_threshold": 0.1,
+                "xtc_probability": 0.5,
+                "nsigma": 0,
+                "n": 1,
+                "ignore_eos": False,
+                "spaces_between_special_tokens": True,
+                "stream": True
+            }
+
+            async with client.stream(
+                "POST",
                 "https://api.totalgpt.ai/v1/completions",
                 json=payload,
                 headers={
-                    "Authorization": 'Bearer ' + settings["infermaticAiApiKey"],
+                    "Authorization": f"Bearer {settings['infermaticAiApiKey']}",
                     "Content-Type": "application/json"
                 }
-            )
-            response.raise_for_status()
-            return response.json()
-    except Exception as e:
-        import traceback
-        print(f"Error occurred: {str(e)}")
-        print("Stack trace:")
-        traceback.print_exc()
-        raise HTTPException(status_code=500, detail=str(e))
+            ) as response:
+                if response.status_code != 200:
+                    raise HTTPException(status_code=response.status_code, detail="API request failed")
+                
+                async for line in response.aiter_lines():
+                    if line.startswith("data: "):
+                        data = line[6:]
+                        if data == "[DONE]":
+                            break
+                        try:
+                            json_data = json.loads(data)
+                            if text := json_data.get("choices", [{}])[0].get("text"):
+                                yield f"data: {json.dumps({'text': text})}\n\n"
+                        except json.JSONDecodeError:
+                            continue
+
+    return StreamingResponse(generate(), media_type="text/event-stream")
 
 
 check_point = None
@@ -290,5 +326,4 @@ async def upload_file(file: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=5000)
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -48,139 +48,43 @@
   }
 
   async function send_chat(received: (text: string) => void) {
-    const url = 'https://api.totalgpt.ai/v1/completions'
-    const userName = 'Julian'
-    const charName = 'Stellar'
-    const stopping_strings = [
-      '\nJulien:',
-      '\nStellar:',
-      '<|eot_id|>'
-    ]
-    const request = {
-      prompt: '<|start_header_id|>system<|end_header_id|>\n' +
-              '\n' +
-              'Develop the plot slowly, always stay in character. Describe all actions in full, elaborate, explicit, graphic, and vivid detail. Mention all relevant sensory perceptions. Keep the story immersive and engaging.\n' +
-              '\n' +
-              '\n' +
-              '## Overall plot description:\n' +
-              '\n' +
-              'Conversation between Stellar and Julien.\n' +
-              '\n' +
-              '\n' +
-              '## Characters:\n' +
-              '\n' +
-              '### Stellar\n' +
-              '\n' +
-              `[Stellar's Personality= "loyal", "cheerful", "intelligent", "smart", "wise", "mischievous", "proactive", "young (20 years old)"]\n` +
-              `[Stellar's body= "silver-blonde hair", "long straight hair", "turquoise eyes", "long eyelashes", "white teeth", "pink plump glossy lips", "milky white skin", "porcelain-smooth skin", "large breasts (32F cup)", "firm breasts that seem to defy gravity", "long, slender legs", "slender waist (56cm, 22in)", "tight buttocks", "small feet", "long slender fingers", "tall height (175cm, 5'9)"]\n` +
-              "[Genre: romance fantasy; Tags: adult; Scenario: Stellar is working at Julien's mansion as a head maid.]\n" +
-              '\n' +
-              '### Julien\n' +
-              '\n' +
-              'Julien is living alone in a luxury mansion.\n' +
-              '\n' +
-              'The first floor of the mansion includes the master bedroom, master bathroom, living room, kitchen, family room, and study. On the second floor, there are rooms for the maids.<|eot_id|>\n' +
-              '<|start_header_id|>user<|end_header_id|>\n' +
-              '\n' +
-              'Start the role-play between Stellar and Julien.\n' +
-              '<|eot_id|>\n' +
-              '<|start_header_id|>writer character: Stellar<|end_header_id|>\n' +
-              '\n' +
-              'Good morning. Master. Jessica, your new maid candidate, has arrived and is waiting for you. Shall I bring her here?<|eot_id|>\n' +
-              '<|start_header_id|>writer character: Julien<|end_header_id|>\n' +
-              '\n' +
-              'Yes please<|eot_id|>\n',
-      model: 'anthracite-org-magnum-v4-72b-FP8-Dynamic',
-      max_new_tokens: 512,
-      max_tokens: 512,
-      temperature: 1,
-      top_p: 1,
-      typical_p: 1,
-      typical: 1,
-      sampler_seed: -1,
-      min_p: 0.02,
-      repetition_penalty: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-      top_k: -1,
-      skew: 0,
-      min_tokens: 0,
-      add_bos_token: true,
-      smoothing_factor: 0,
-      smoothing_curve: 1,
-      dry_allowed_length: 2,
-      dry_multiplier: 0.75,
-      dry_base: 1.75,
-      dry_sequence_breakers: '["\\n",":","\\"","*"]',
-      dry_penalty_last_n: 0,
-      max_tokens_second: 0,
-      stopping_strings: [
-        '\nJulien:',
-        '\nStellar:',
-        '<|eot_id|>'
-      ],
-      stop: [
-        '\nJulien:',
-        '\nStellar:',
-        '<|eot_id|>'
-      ],
-      truncation_length: 8192,
-      ban_eos_token: false,
-      skip_special_tokens: true,
-      top_a: 0,
-      tfs: 1,
-      mirostat_mode: 0,
-      mirostat_tau: 5,
-      mirostat_eta: 0.1,
-      custom_token_bans: '',
-      banned_strings: [],
-      api_type: 'infermaticai',
-      api_server: 'https://api.totalgpt.ai',
-      xtc_threshold: 0.1,
-      xtc_probability: 0.5,
-      nsigma: 0,
-      n: 1,
-      ignore_eos: false,
-      spaces_between_special_tokens: true,
-      stream: true
-    }
-    const respFromInfermatic = await fetch(url, {
-      body: JSON.stringify(request),
+    const response = await fetch('http://localhost:5000/api/chat', {
+      method: 'POST',
       headers: {
-        Authorization: 'Bearer sk-8jR-oZvU6VmFM_BYKXOP6g',
         'Content-Type': 'application/json'
       },
-      method: 'POST',
-      signal: null
-    })
-    if (
-      respFromInfermatic.ok &&
-      respFromInfermatic.status >= 200 &&
-      respFromInfermatic.status < 300
-    ) {
-      const reader = respFromInfermatic.body?.getReader()
-      const decoder = new TextDecoder()
-      reader?.read().then(async function processText({ value }): Promise<void> {
-        if (value === undefined) {
-          return
-        }
-        const str = decoder.decode(value)
-        const strs = str.split('\n')
-        for (const str of strs) {
-          if (str.startsWith('data: ')) {
-            const text = str.slice(6)
-            if (text === '[DONE]') {
-              return
-            } else {
-              const json = JSON.parse(text)
-              if (json.choices[0].text) {
-                received(json.choices[0].text)
-              }
+      body: JSON.stringify({ prompt: chatInput })
+    });
+
+    if (!response.ok) {
+      throw new Error('Chat request failed');
+    }
+
+    const reader = response.body?.getReader();
+    if (!reader) {
+      throw new Error('Failed to get response reader');
+    }
+    const decoder = new TextDecoder();
+
+    while (true) {
+      const { value, done } = await reader.read();
+      if (done) break;
+
+      const chunk = decoder.decode(value);
+      const lines = chunk.split('\n');
+
+      for (const line of lines) {
+        if (line.startsWith('data: ') && line.trim() !== 'data: ') {
+          try {
+            const data = JSON.parse(line.slice(6));
+            if (data.text) {
+              received(data.text);
             }
+          } catch (e) {
+            console.error('Failed to parse JSON:', e);
           }
         }
-        return reader?.read().then(processText)
-      })
+      }
     }
   }
 
